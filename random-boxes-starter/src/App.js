b7d32ca60c4ace89c0041c2cffc5d15e45a98190
Himanshu Arora <himanshuarora9419@gmail.com>
2017-11-14 20:52:16 +0530

    my solution to color box

---
+++ random-boxes-starter/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+import ColorBox from './ColorBox';
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    var color=[];
+    for(var i=0;i<64;i++){color.push(this.props.allColors[Math.floor(Math.random()*this.props.allColors.length)])}
+    this.state={
+      color
+    }
+    setInterval(()=>{
+      var index=Math.floor(Math.random()*this.state.color.length);
+      var colorIndex = Math.floor(Math.random() * this.props.allColors.length);
+        const color=this.state.color.slice();
+        color[index]=this.props.allColors[colorIndex];
+        this.setState({color});
+    },1000)
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
+        {this.state.color.map((elem,i)=> (<ColorBox key={i}  color={elem}></ColorBox>) )}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
+
+export default App;
---
+++ random-boxes-starter/src/ColorBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ColorBox=props=>{
+    return (
+        <div style={{'backgroundColor':props.color,'height':'12.5%','width':'12.5%','display':'flex'}}>
+        </div>
+    )
+}
+
+export default ColorBox;
